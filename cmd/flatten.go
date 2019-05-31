c1c85d6cacc6c7af03793bae15be295683df3b51
Bryan Strassner <strassner.bryan@gmail.com>
2019-05-30 22:51:28 -0500

    Add basic flatten functionality
    Flatten functionality will now take a concatenated yaml input file and
    produce output as a folder/file structure at the desired output
    location.

--- cmd/flatten.go
+++ cmd/flatten.go
@@ -15,6 +15,9 @@ import (
 var InputFile string
 var OutputDir string
 
+const fileModeForDir = os.FileMode(int(0744))
+const fileModeForCreated = os.FileMode(int(0664))
+
 func init() {
 	rootCmd.AddCommand(flattenCmd)
 	wd, _ := os.Getwd()
@@ -43,7 +46,6 @@ as folders per schema`,
 		// Resolve input and output, such that the input file name is either
 		// absolute or relative and ends up being absolute
 
-		//TODO: encapsulate things into funcs
 		inputFileName := args[0]
 		if !strings.HasPrefix(args[0], "/") {
 			wd, _ := os.Getwd()
@@ -51,38 +53,71 @@ as folders per schema`,
 		}
 		_, inFileName := path.Split(inputFileName)
 		inFileName = strings.Split(inFileName, ".")[0]
-		outputFileName := path.Join(cmd.Flag("output").Value.String(), inFileName)
+		outputFileDir := path.Join(cmd.Flag("output").Value.String(), inFileName)
 		fmt.Printf("Flatten Input is %s\n", inputFileName)
-		fmt.Printf("Flatten Output is %s\n", outputFileName)
+		fmt.Printf("Flatten Output is %s\n", outputFileDir)
 
-		file, _ := os.Open(inputFileName)
-		file.Close()
-		yamlIn, err := ioutil.ReadFile(inputFileName)
-		if err != nil {
-			fmt.Printf("Yaml file could not be read #%v ", err)
-		}
-
-		decoder := yaml.NewDecoder(bytes.NewReader(yamlIn))
+		decoder := yaml.NewDecoder(bytes.NewReader(readYamlInput(inputFileName)))
 		// use a Node for demarshalling so that we can keep the whole doc
 		// Decode to a YF struct to allow for directing the output
 		var yn yaml.Node
-		var yf YF
 
 		for decoder.Decode(&yn) == nil {
+			var yf YF
 			err := yn.Decode(&yf)
 			if err != nil {
-				fmt.Printf("#%v", err)
+				fmt.Printf("#%v\n", err)
 			}
 			fmt.Printf("%s with name: %s and layer: %s\n", yf.Schema, yf.Metadata.Name, yf.Metadata.LayeringDefinition.Layer)
-			//TODO: Create directories based on the schema
-			//TODO: Write files based on the name-layer
 			yba, err := yaml.Marshal(&yn)
 			if err != nil {
-				fmt.Printf("#%v", err)
+				fmt.Printf("#%v \n", err)
 			}
-			fmt.Println(string(yba))
+			writeYamlToFolder(outputFileDir, yf.Schema, yf.Metadata.Name, yf.Metadata.LayeringDefinition.Layer, yba)
 		}
 		fmt.Println("Done.")
-
 	},
+}
+
+// Reads the input file into a []byte
+func readYamlInput(inputFileName string) (yamlIn []byte) {
+	yamlIn, err := ioutil.ReadFile(inputFileName)
+	if err != nil {
+		fmt.Printf("Yaml file could not be read #%v \n", err)
+	}
+	return yamlIn
+}
+
+// Convert a input string schema to a folder name by converting all "/" characters to "."
+func schemaToFolderName(schema string) (folder string) {
+	if strings.TrimSpace(schema) == "" {
+		folder = "unknown"
+	} else {
+		folder = strings.ReplaceAll(schema, "/", ".")
+	}
+	return folder
+}
+
+// Create directories using the base path and schema name
+func createFolderFromSchema(baseFolderPath string, schema string) {
+	os.MkdirAll(path.Join(baseFolderPath, schemaToFolderName(schema)), fileModeForDir)
+}
+
+// Creates a file name of <docname>-<doclayer>
+func docnameToFileName(docname string, doclayer string) (name string) {
+	name = strings.Join([]string{docname, doclayer}, "-")
+	return name
+}
+
+// Writes a []byte to baseFolderPath/schema(translated)/docname(translated)
+func writeYamlToFolder(baseFolderPath string, schema string, docname string, doclayer string, yamlBytes []byte) {
+	createFolderFromSchema(baseFolderPath, schema)
+	filename := path.Join(baseFolderPath, schemaToFolderName(schema), docnameToFileName(docname, doclayer))
+	fullYaml := append(append([]byte("---\n"), yamlBytes...), []byte("...\n")...)
+	err := ioutil.WriteFile(filename, fullYaml, fileModeForCreated)
+	if err != nil {
+		fmt.Printf("#%v \n", err)
+	} else {
+		fmt.Printf("Wrote %s\n", filename)
+	}
 }
